2355157bf0f4588645cbf700085e5b3c5c9be472
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-21 16:04:45 +0200

    specify main dns name as common name

--- certificate.tf
+++ certificate.tf
@@ -4,6 +4,7 @@ locals {
     issuer_kind               = var.cert_manager_issuer_kind
     issuer_name               = var.cert_manager_issuer_name
     dns_names                 = [local.atlantis_url]
+    atlantis_url              = local.atlantis_url
     app_name                  = local.release_name
     k8s_namespace             = var.k8s_namespace
   })
