be1f2c854881f53fb7db059f80d104d85f6e399b
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-24 02:35:36 +0200

    remove custom secrets for now

--- secrets.tf
+++ secrets.tf
@@ -27,33 +27,11 @@ data "aws_lambda_invocation" "decrypt" {
 }
 
 locals {
-  secret_keys            = keys(var.lambda_custom_secrets)
-  secrets_name           = length(local.secret_keys) > 0 ? format("%s-custom", local.release_name) : ""
-  decrypt_secrets_result = length(local.secret_keys) > 0 ? data.aws_lambda_invocation.decrypt_additional.0.result_map : {}
-  decrypt_result         = data.aws_lambda_invocation.decrypt.*.result_map
+  decrypt_result = data.aws_lambda_invocation.decrypt.*.result_map
 
   sensitives = {
     user   = join("", coalescelist(local.decrypt_result.*.user, data.aws_ssm_parameter.user.*.value, [var.encrypted_user]))
     token  = join("", coalescelist(local.decrypt_result.*.token, data.aws_ssm_parameter.token.*.value, [var.encrypted_token]))
     secret = join("", coalescelist(local.decrypt_result.*.secret, data.aws_ssm_parameter.secret.*.value, [var.encrypted_secret]))
-  }
-}
-
-data "aws_lambda_invocation" "decrypt_additional" {
-  count         = length(local.secret_keys) > 0 ? 1 : 0
-  function_name = var.lambda_encryption_function
-
-  input = jsonencode({
-    map = var.lambda_custom_secrets
-  })
-}
-
-resource "kubernetes_secret" "custom_secrets" {
-  count = length(local.secret_keys) > 0 ? 1 : 0
-  data  = local.decrypt_secrets_result
-
-  metadata {
-    name      = local.secrets_name
-    namespace = var.k8s_namespace
   }
 }
