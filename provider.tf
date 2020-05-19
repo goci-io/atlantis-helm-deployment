ea8cebd970de406286b9c829ef7144901f45513c
etwillbefine <tim.gatzemeier@gmx.de>
2020-05-19 13:46:38 +0200

    remove provider config from module

--- provider.tf
+++ provider.tf
@@ -2,17 +2,9 @@ terraform {
   required_version = ">= 0.12.1"
 
   required_providers {
+    aws        = "~> 2.50"
     helm       = "~> 1.0"
     random     = "~> 2.2"
     kubernetes = "~> 1.11"
-  }
-}
-
-provider "aws" {
-  version = "~> 2.45"
-  region  = var.aws_region
-
-  assume_role {
-    role_arn = var.aws_assume_role_arn
   }
 }
