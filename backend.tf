a4fe5c10f81b4678207f0130b65014d1ae4e30db
etwillbefine <tim.gatzemeier@gmx.de>
2020-08-29 14:23:28 +0200

    rename state and lock outputs, add bucket id output

--- backend.tf
+++ backend.tf
@@ -1,16 +1,8 @@
 module "state_backend" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.18.2"
+  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.23.0"
   region     = var.aws_region
   namespace  = var.namespace
   stage      = var.stage
   name       = local.release_name
   attributes = ["terraform", "state", var.region]
-}
-
-output "state_backend_s3_bucket_arn" {
-  value = module.state_backend.s3_bucket_arn
-}
-
-output "state_backend_dynamodb_table_arn" {
-  value = module.state_backend.dynamodb_table_arn
 }
